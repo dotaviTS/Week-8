7d66e313d4001193197188337dc6614fa1bce4d5
Haseenah01 <haseenahsami27@gmail.com>
2023-08-23 13:02:28 +0200

    wind speed and pressure  metrics added

--- Haseenah_Sami/Example_5.4_Telemetry/weather_station/lib/weather_station.ex
+++ Haseenah_Sami/Example_5.4_Telemetry/weather_station/lib/weather_station.ex
@@ -12,7 +12,18 @@ defmodule WeatherStation do
       :world
 
   """
-  def hello do
-    :world
+  # def hello do
+  #   :world
+  # end
+
+  def collect_data do
+    temperature = Enum.random(15..35)
+    humidity = Enum.random(30..70)
+
+    # Exercise 5.5: Implement System Monitoring
+    wind_speed = Enum.random(20..120)
+    pressure = Enum.random(0..12)
+
+    :telemetry.execute([:weather, :data], %{temperature: temperature, humidity: humidity, wind_speed: wind_speed, pressure: pressure}, %{})
   end
 end
