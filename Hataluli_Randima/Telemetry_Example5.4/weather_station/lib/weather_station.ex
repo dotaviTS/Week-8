3fecba6dd3257010279ae093f99c31564e554704
HataluliRandima <219017847@student.uj.ac.za>
2023-08-23 12:05:19 +0200

    Added collect data method

--- Hataluli_Randima/Telemetry_Example5.4/weather_station/lib/weather_station.ex
+++ Hataluli_Randima/Telemetry_Example5.4/weather_station/lib/weather_station.ex
@@ -15,4 +15,11 @@ defmodule WeatherStation do
   def hello do
     :world
   end
+
+  def collect_data do
+    temperature = Enum.random(15..35)
+    humidity = Enum.random(30..70)
+
+    :telemetry.execute([:weather, :data], %{temperature: temperature, humidity: humidity}, %{})
+  end
 end
