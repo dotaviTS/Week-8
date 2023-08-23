4318fdf9d48df266b86eee339329e21a926a1893
HataluliRandima <219017847@student.uj.ac.za>
2023-08-23 12:38:44 +0200

    Addeed the mesurements on the handler

--- Hataluli_Randima/Telemetry_Example5.4/weather_station/lib/WeatherStation/telemetry_handler.ex
+++ Hataluli_Randima/Telemetry_Example5.4/weather_station/lib/WeatherStation/telemetry_handler.ex
@@ -2,7 +2,7 @@ defmodule WeatherStation.TelemetryHandler do
   require Logger
 
   def handle_event([:weather, :data], measurements, _metadata, _config) do
-    Logger.info("Collected weather data: Temperature #{measurements.temperature}°C, Humidity #{measurements.humidity}%")
+    Logger.info("Collected weather data: Temperature #{measurements.temperature}°C, Humidity #{measurements.humidity}%, WindSpeed #{measurements.windspeed}kmh , Pressure #{measurements.pressure} Pa")
     :telemetry.attach("weather-data-handler", [:weather, :data], &WeatherStation.TelemetryHandler.handle_event/4, %{})
   end
 
