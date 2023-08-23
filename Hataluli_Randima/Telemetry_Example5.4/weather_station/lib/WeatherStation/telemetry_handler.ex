3803656a31002860debb458263485753e0faf890
HataluliRandima <219017847@student.uj.ac.za>
2023-08-23 12:34:34 +0200

    Added exercise 5.5 for adding more metrics on weather station

---
+++ Hataluli_Randima/Telemetry_Example5.4/weather_station/lib/WeatherStation/telemetry_handler.ex
@@ -0,0 +1,11 @@
+defmodule WeatherStation.TelemetryHandler do
+  require Logger
+
+  def handle_event([:weather, :data], measurements, _metadata, _config) do
+    Logger.info("Collected weather data: Temperature #{measurements.temperature}°C, Humidity #{measurements.humidity}%")
+    :telemetry.attach("weather-data-handler", [:weather, :data], &WeatherStation.TelemetryHandler.handle_event/4, %{})
+  end
+
+  # :telemetry.attach("weather-data-handler", [:weather, :data], &WeatherStation.TelemetryHandler.handle_event/4, %{})
+
+end
