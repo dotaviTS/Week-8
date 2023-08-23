340651d5d98ed1aa09b76947e7f39ffce9eb2886
Kevin-Naidoo <kevinnelsonnaidoo@gmail.com>
2023-08-23 20:30:36 +0200

    Weather Project created

---
+++ Kevin_Naidoo/Telemetry_Example/weather_station/lib/weather_station.ex
@@ -0,0 +1,18 @@
+defmodule WeatherStation do
+  @moduledoc """
+  Documentation for `WeatherStation`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> WeatherStation.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
