270a5c95f0190f7afa11bbc7e9384b9242b05a41
ShawnMumbo <shawn.mumbo@tauspace.com>
2023-08-24 13:33:17 +0200

    add more measurements

--- Shawn_Mumbo/Telemetry/weather_station/lib/weather_station.ex
+++ Shawn_Mumbo/Telemetry/weather_station/lib/weather_station.ex
@@ -2,7 +2,10 @@ defmodule WeatherStation do
   def collect_data do
     temperature = Enum.random(15..35)
     humidity = Enum.random(30..70)
+    windspeed = Enum.random(5..253)
+    precipitation = Enum.random(20..140)
+    pressure = Enum.random(5..31)
 
-    :telemetry.execute([:weather, :data], %{temperature: temperature, humidity: humidity}, %{})
+    :telemetry.execute([:weather, :data], %{temperature: temperature, humidity: humidity, windspeed: windspeed, precipitation: precipitation, pressure: pressure}, %{})
   end
 end
