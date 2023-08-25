937041c76a208062e2c3fa30db143145598b0914
Haseenah01 <haseenahsami27@gmail.com>
2023-08-25 10:59:26 +0200

    counter example added

---
+++ Haseenah_Sami/5.2_Counter_example/counter/lib/counter.ex
@@ -0,0 +1,37 @@
+defmodule Counter do
+  use GenServer
+
+  # Callbacks
+def init(initial_count) do
+    {:ok, initial_count}
+  end
+
+def handle_call(:increment, _from, state) do
+  {:reply, state, state + 1}
+end
+
+def handle_call(:decrement, _from, state) do
+  {:reply, :ok, state - 1}
+end
+
+def handle_call(:get, _from, state) do
+  IO.puts state
+  {:reply, state, state}
+end
+def start_link(init_value \\ 0) do
+  GenServer.start_link(__MODULE__, init_value, name: __MODULE__)
+end
+# Client API
+
+def increment do
+  GenServer.call(__MODULE__, :increment)
+end
+
+def decrement do
+  GenServer.call(__MODULE__, :decrement)
+end
+
+def get do
+  GenServer.call(__MODULE__, :get)
+end
+end
