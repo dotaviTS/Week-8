6aa67f5521958eeb7ddbcf68f667ed75b082f081
HataluliRandima <219017847@student.uj.ac.za>
2023-08-25 13:51:29 +0200

    Created banking app

---
+++ Hataluli_Randima/TsBank_Features/tsbank_app/lib/tsbank_app.ex
@@ -0,0 +1,18 @@
+defmodule TsbankApp do
+  @moduledoc """
+  Documentation for `TsbankApp`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TsbankApp.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
