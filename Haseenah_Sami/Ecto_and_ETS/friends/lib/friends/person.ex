0e5c3aac7542e0f71eb4890a0d9fbccde12c72e8
Haseenah01 <haseenahsami27@gmail.com>
2023-08-25 21:02:32 +0200

    changeset added in schema file

--- Haseenah_Sami/Ecto_and_ETS/friends/lib/friends/person.ex
+++ Haseenah_Sami/Ecto_and_ETS/friends/lib/friends/person.ex
@@ -1,8 +1,15 @@
 defmodule Friends.Person do
   use Ecto.Schema
+  import Ecto.Changeset
 
   schema "people" do
     field :name, :string
     field :age, :integer, default: 0
+  end
+
+  def changeset(struct, params) do
+    struct
+    |> cast(params, [:name, :age])
+    |> validate_required([:name])
   end
 end
