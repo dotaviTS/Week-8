ceca59ab355ff5a563606d19c3951c3aab9ed883
HataluliRandima <219017847@student.uj.ac.za>
2023-08-26 11:04:23 +0200

    Added some instructions and deps

---
+++ Hataluli_Randima/Ecto_and_ETS/friends/config/config.exs
@@ -0,0 +1,7 @@
+import Config
+
+config :friends,ecto_repos: [Friends.Repo],
+  database: "friends_repo",
+  username: "postgres",
+  password: "Hata@randima.00",
+  hostname: "localhost"
--- Hataluli_Randima/Ecto_and_ETS/friends/lib/friends/application.ex
+++ Hataluli_Randima/Ecto_and_ETS/friends/lib/friends/application.ex
@@ -8,6 +8,7 @@ defmodule Friends.Application do
   @impl true
   def start(_type, _args) do
     children = [
+      Friends.Repo,
       # Starts a worker by calling: Friends.Worker.start_link(arg)
       # {Friends.Worker, arg}
     ]
