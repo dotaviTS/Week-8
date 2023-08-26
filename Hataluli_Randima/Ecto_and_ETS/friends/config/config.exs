1dfdf65a0f7d05cb3df6d80af0e8a68dc6124c71
HataluliRandima <219017847@student.uj.ac.za>
2023-08-26 11:14:26 +0200

    added ecto repos to run ecto commands

--- Hataluli_Randima/Ecto_and_ETS/friends/config/config.exs
+++ Hataluli_Randima/Ecto_and_ETS/friends/config/config.exs
@@ -1,6 +1,7 @@
 import Config
 
-config :friends,ecto_repos: [Friends.Repo],
+config :friends, ecto_repos: [Friends.Repo]
+config :friends, Friends.Repo,
   database: "friends_repo",
   username: "postgres",
   password: "Hata@randima.00",
