cacd15f8ef8771bfb1ffd6671fc92078847b47a1
HataluliRandima <219017847@student.uj.ac.za>
2023-08-26 22:14:52 +0200

    created actor schema

--- Hataluli_Randima/Ecto_and_ETS/friends/lib/friends/movie.ex
+++ Hataluli_Randima/Ecto_and_ETS/friends/lib/friends/movie.ex
@@ -7,6 +7,7 @@ defmodule Friends.Movie do
     field :tagline, :string
     has_many :characters, Friends.Character
     has_one :distributor, Friends.Distributor # I'm new!
+    many_to_many :actors, Friends.Actor, join_through: "movies_actors" # I'm new!
   end
 
 end
