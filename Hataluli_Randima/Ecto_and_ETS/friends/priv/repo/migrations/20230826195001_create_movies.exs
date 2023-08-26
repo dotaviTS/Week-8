b00897ae08f074de00b9091d6344cdf0f2d311da
HataluliRandima <219017847@student.uj.ac.za>
2023-08-26 21:53:33 +0200

    created movies table

--- Hataluli_Randima/Ecto_and_ETS/friends/priv/repo/migrations/20230826195001_create_movies.exs
+++ Hataluli_Randima/Ecto_and_ETS/friends/priv/repo/migrations/20230826195001_create_movies.exs
@@ -2,6 +2,10 @@ defmodule Friends.Repo.Migrations.CreateMovies do
   use Ecto.Migration
 
   def change do
-
+   create table(:movies) do
+    add :title, :string
+    add :tagline, :string
+    has_many :characters, Friends.Character
+   end
   end
 end
